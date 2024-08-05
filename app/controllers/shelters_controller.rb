143d003d0392d3f0dfc784072c83369169ee3b30
Masashi <masashi.nkys@gmail.com>
2024-08-05 17:25:32 +0900

    Organize controllers on every mdoels

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -1,22 +1,70 @@
-class SheltersController < ApplicationController
+class SheltersController < ApplicationController 
+  before_action :set_shelter, only: %i[ show edit update destroy ]
+
+  # GET /shelters or /shelters.json
   def index
+    @shelters = Shelter.all
   end
 
+  # GET /shelters/1 or /shelters/1.json
   def show
   end
 
+  # GET /shelters/new
   def new
+    @shelter = Shelter.new
   end
 
-  def create
-  end
-
+  # GET /shelters/1/edit
   def edit
   end
 
-  def update
+  # POST /shelters or /shelters.json
+  def create
+    @shelter = Shelter.new(shelter_params)
+
+    respond_to do |format|
+      if @shelter.save
+        format.html { redirect_to shelter_url(@shelter), notice: "shelter was successfully created." }
+        format.json { render :show, status: :created, location: @shelter }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @shelter.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def destroy
+  # PATCH/PUT /shelters/1 or /shelters/1.json
+  def update
+    respond_to do |format|
+      if @shelter.update(shelter_params)
+        format.html { redirect_to shelter_url(@shelter), notice: "shelter was successfully updated." }
+        format.json { render :show, status: :ok, location: @shelter }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @shelter.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # DELETE /shelters/1 or /shelters/1.json
+  def destroy
+    @shelter.destroy
+
+    respond_to do |format|
+      format.html { redirect_to shelters_url, notice: "shelter was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_shelter
+      @shelter = Shelter.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def shelter_params
+      params.require(:shelter).permit(:name, :location_address, :contact_info)
+    end
 end
--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -1,22 +1,70 @@
 class SuppliesController < ApplicationController
+  before_action :set_supply, only: %i[ show edit update destroy ]
+
+  # GET /supplies or /supplies.json
   def index
+    @supplies = Supply.all
   end
 
+  # GET /supplies/1 or /supplies/1.json
   def show
   end
 
+  # GET /supplies/new
   def new
+    @supply = Supply.new
   end
 
-  def create
-  end
-
+  # GET /supplies/1/edit
   def edit
   end
 
-  def update
+  # POST /supplies or /supplies.json
+  def create
+    @supply = Supply.new(supply_params)
+
+    respond_to do |format|
+      if @supply.save
+        format.html { redirect_to supply_url(@supply), notice: "supply was successfully created." }
+        format.json { render :show, status: :created, location: @supply }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @supply.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
-  def destroy
+  # PATCH/PUT /supplies/1 or /supplies/1.json
+  def update
+    respond_to do |format|
+      if @supply.update(supply_params)
+        format.html { redirect_to supply_url(@supply), notice: "supply was successfully updated." }
+        format.json { render :show, status: :ok, location: @supply }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @supply.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  # DELETE /supplies/1 or /supplies/1.json
+  def destroy
+    @supply.destroy
+
+    respond_to do |format|
+      format.html { redirect_to supplies_url, notice: "supply was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_supply
+      @supply = Supply.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def supply_params
+      params.require(:supply).permit(:name, :description, :unit, :category_id)
+    end
 end
